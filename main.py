5819f363dce2ea0d996dd4e8511818d3c819ab3b
romas <r2udis@gmail.com>
2025-06-05 18:28:29 +0300

    add tree builder

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+from node import Node, build_tree, sort_nodes
+
+from temp import *
 
 def main():
-    print("hi")
+    data_str = test_data
+    nodes = []
+
+    for char in data_str:
+        ascii_code = ord(char)
+        found = False
+
+        for node in nodes:
+            if node.ascii_code == ascii_code:
+                node.count += 1
+                found = True
+                break
+
+        if not found:
+            nodes.append(Node(1, ascii_code))
+
+    sorted_nodes = sort_nodes(nodes)
+    root_node = build_tree(sorted_nodes)
+
+    print_tree_pretty(root_node)
+    print(f"len: {len(test_data)}")
 
 if __name__ == "__main__":
     main()
--- node.py
+++ node.py
@@ -9,3 +9,18 @@ class Node:
 
     def __str__(self):
         return f"<{self.ascii_code}> <{self.count}>"
+
+def build_tree(nodes):
+    if len(nodes) == 1:
+        return nodes[0]
+
+    new_nodes = []
+    node_1 = nodes.pop(0)
+    node_2 = nodes.pop(0)
+    new_nodes.append(Node(node_1.count + node_2.count, None, node_1, node_2))
+    new_nodes.extend(nodes)
+
+    return build_tree(sort_nodes(new_nodes))
+
+def sort_nodes(nodes):
+    return sorted(nodes, key=lambda node: node.count)
